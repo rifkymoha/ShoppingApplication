97f9b73c72a6f018af69254762bfadafef44dec1
Rifky <mohamedrifky534@gmail.com>
2022-09-08 11:22:57 +0530

    Add project files.

---
+++ Shoppingapplication/Controllers/Item.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Shoppingapplication.Models;
+
+namespace Shoppingapplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Item : ControllerBase
+    {
+        private readonly IMongoCollection<Items> _items;
+        public Item()
+        {
+            var dbHost = "host.docker.internal";
+            var dbName = "item_order";
+            var connectionString = $"mongodb://{dbHost}:27017/{dbName}";
+            var mongoUrl = MongoUrl.Create(connectionString);
+            var mongoClient = new MongoClient(mongoUrl);
+            var database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+            _items = database.GetCollection<Items>("items");
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Items>>> GetItems()
+        {
+            return await _items.Find(Builders<Items>.Filter.Empty).ToListAsync();
+        }
+
+        [HttpGet("{ItemId}")]
+
+        public async Task<ActionResult<Items>> GetById(string itemId)
+        {
+            var filterDefinition = Builders<Items>.Filter.Eq( x => x.ItemId, itemId);
+            return await _items.Find(filterDefinition).SingleOrDefaultAsync();
+
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> Create(Items items)
+        {
+            await _items.InsertOneAsync(items);
+            return Ok();
+        }
+
+
+
+
+        [HttpPut]
+        public async Task<ActionResult> Update(Items items)
+        {
+            var filterDefinition = Builders<Items>.Filter.Eq(x => x.ItemId, items.ItemId);
+            await _items.ReplaceOneAsync(filterDefinition, items);
+            return Ok();
+        }
+
+        [HttpDelete("{ItemId}")]
+        public async Task<ActionResult> Delete(string itemId)
+        {
+            var filterDefinition = Builders<Items>.Filter.Eq(x => x.ItemId, itemId);
+            await _items.DeleteOneAsync(filterDefinition);
+            return Ok();
+        }
+
+    }
+}
---
+++ Shoppingapplication/Models/Items.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Shoppingapplication.Models
+{
+    [Serializable, BsonIgnoreExtraElements]
+    public class Items
+    {
+        [BsonId] //, BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public String ItemId { get; set; }
+
+      
+        public string Itemname { get; set; }
+
+        
+        public string Discription { get; set; }
+
+     
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
